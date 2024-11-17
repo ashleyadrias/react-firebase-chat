6cf82ecf8dc427597907331ff243bd0fef590cd4
Ashley Adrias <agadrias@gmail.com>
2024-11-17 07:36:25 -0800

    new components

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,112 @@
-import "./chat.css"
+import { useState } from 'react';
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
+
+    const [open, setOpen] = useState(false);
+    const [text, setText] = useState("");
+
+    const handleEmoji = e =>{
+        console.log(e)
+        setText((prev)=>prev+e.emoji);
+        setOpen(false)
+    }
+
+    console.log(text)
+
     return (
-        <div className='chat'> Chat
+        <div className='chat'> 
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt=""></img>
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt=""/>
+                    <img src="./video.png" alt=""/>
+                    <img src="./info.png" alt=""/>
+                </div>
+            </div>
+            <div className="center">
+                <div className="message">
+                    <img src="avatar.png" alt=""/>
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="avatar.png" alt=""/>
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="avatar.png" alt=""/>
+                    <div className="texts">
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <img src="https://www.pexels.com/photo/orange-usb-charger-cable-on-black-surface-4219862/" alt=""></img>
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. </p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png" alt=""/>
+                    <img src="./camera.png" alt=""/>
+                    <img src="./mic.png" alt=""/>
+                </div>
+                <input 
+                    type="text" 
+                    placeholder="Type a message"
+                    value = {text}
+                    onChange={e=>setText(e.target.value)}></input>
+                <div className="emoji">
+                    <img 
+                        src="./emoji.png" 
+                        alt="" 
+                        onClick = {()=>setOpen((prev)=>!prev)}/>
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
+                </div>
+                <button className="sendButton">Send</button>
+            </div>
         </div>
     )
 }
